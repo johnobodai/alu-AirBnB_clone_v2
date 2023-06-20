822a557aa36ecd80ba9df3fe74ab5c5686fc13c3
johnobodai <j.obodai@alustudent.com>
2023-06-20 12:43:06 +0000

    Bash script to set up web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
-# Bash script  to set up web servers for the deployment of web_static
+
+# Bash script to set up web servers for the deployment of web_static
 
 # Install Nginx if not already installed
-if ! command -v nginx >/dev/null 2&1; then
+if ! command -v nginx >/dev/null 2>&1; then
     sudo apt-get update
     sudo apt-get -y install nginx
 fi
+
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
+sudo echo "Test Page" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link and update ownership
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
 

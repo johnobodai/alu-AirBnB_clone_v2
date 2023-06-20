e57250a9d39736e3adea4ed9b7681dff6f3df68d
johnobodai <j.obodai@alustudent.com>
2023-06-20 14:05:33 +0000

    Edit fabric sript to check errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,96 +1,67 @@
 #!/usr/bin/python3
+'''
+fabric script to distribute an archive to web servers
+'''
 
-
-from fabric.api import local, put, run, env
-from datetime import datetime
 import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
-# Define the hosts
-env.hosts = ['localhost']
 
-def do_pack():
-    """Generate a .tgz archive from the contents of the web_static folder"""
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
+env.hosts = ['34.138.32.248', '3.226.74.205']
 
-    archive_path = "versions/web_static_{}.tgz".format(timestamp)
-
-    # Create the versions folder if it doesn't exist
-    local("mkdir -p versions")
-
-    # Generate the .tgz archive
-    result = local("tar -czvf {} web_static".format(archive_path))
-
-    if result.failed:
-        return None
-    else:
-        return archive_path
 
 def do_deploy(archive_path):
-    """Distribute an archive to the web servers"""
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
     if not os.path.exists(archive_path):
         return False
-
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
     try:
-        # Upload the archive to /tmp/ directory
-        put(archive_path, '/tmp/')
-
-        # Extract the archive to the current directory
-        filename = os.path.basename(archive_path)
-        local('mkdir -p temp_folder')
-        local('tar -xzf /tmp/{} -C temp_folder'.format(filename))
-
-        # Delete the archive from the local machine
-        local('rm /tmp/{}'.format(filename))
-
-        # Delete the existing symbolic link
-        local('rm -f hbnb_static')
-
-        # Move the contents of temp_folder to the current directory
-        local('mv temp_folder/* .')
-        local('rm -rf temp_folder')
-
-        # Create a symbolic link to the new version
-        local('ln -s {} hbnb_static'.format(os.path.splitext(filename)[0]))
-
-        return True
-    except:
-        return False
-
-def deploy():
-    """Pack and deploy the web_static folder"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
-
-def clean():
-    """Clean up the local directory"""
-    local('rm -rf versions/*')
-    local('rm -rf hbnb_static')
-
-def do_deploy_web():
-    """Deploy web_static to web servers"""
-    if not deploy():
-        return False
-
-    # Upload the index file to the web servers
-    put('web_static/0-index.html', '/tmp/')
-    run('mkdir -p /data/web_static/releases/test/')
-    run('mv /tmp/0-index.html /data/web_static/releases/test/')
-
-    # Update the symbolic link to the new version
-    run('rm -f /data/web_static/current')
-    run('ln -s /data/web_static/releases/test/ /data/web_static/current')
-
-    # Validate the deployment
-    output_web01 = run('ls /data/web_static/releases/test/')
-    output_web02 = run('ls /data/web_static/releases/test/')
-    if '0-index.html' in output_web01 and '0-index.html' in output_web02:
-        return True
-    else:
-        return False
-
-def pep8():
-    """Run PEP8 validation"""
-    local('pep8 --first --exclude=tests/* fabfile.py')
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
